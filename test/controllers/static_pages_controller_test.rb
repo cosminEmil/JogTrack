debc02abe6868a6f8b0e627f26be41aa600cb743
Bucur Cosmin Emil <cosminbucur2312@gmail.com>
2024-07-24 19:44:27 +0300

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "JogTrack | Run & Track"
   end
 end
