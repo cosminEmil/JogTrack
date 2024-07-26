9e613d1cc2d4560d051dff98d81817a5d8662f9f
Bucur Cosmin Emil <cosminbucur2312@gmail.com>
2024-07-26 16:03:58 +0300

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   resources :users
 end
