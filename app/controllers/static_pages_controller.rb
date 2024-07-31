93d802e00163b5a83d6fdebd925173090b5f2839
Bucur Cosmin Emil <cosminbucur2312@gmail.com>
2024-07-31 16:05:52 +0300

    Implement date range filter for activity feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,14 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @user = current_user
       @activity = current_user.activities.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+
+      if params[:start_date].present? && params[:end_date].present?
+        start_date = Date.parse(params[:start_date])
+        end_date = Date.parse(params[:end_date])
+        @feed_items = current_user.feed.where(run_at: start_date.beginning_of_day..end_date.end_of_day).paginate(page: params[:page])
+      else
+        @feed_items = current_user.feed.paginate(page: params[:page])
+      end
     end
   end
 
